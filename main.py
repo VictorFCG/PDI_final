9a9285f8a6a51967e9e8eb65c9fb7b3a701f9000
Catarina Tecchio Godinho <ctecchiog@gmail.com>
2024-06-19 11:08:54 -0300

    film grain

--- main.py
+++ main.py
@@ -12,6 +12,13 @@ bloom_threshold = 0.5
 bloom_cap = 0.12
 #scanline intensidade
 sl_intensity=0.65
+# Porcentagem da imagem que terá grãos
+grain_amount = 0.0075
+# Tamanho médio dos grãos  
+grain_size = 0.5
+# Intensidade dos grãos
+grain_intensity = 15
+
 
 def bloom(input_img):
     base = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
@@ -113,18 +120,45 @@ def scanline(img):
     # Itera de duas em duas linhas
     for i in range(0, scanline_img.shape[0], 2):
         # Diminui o brilho da linha
-        scanline_img[i] = scanline_img[i] * sl_intensity
-    
+        scanline_img[i] = scanline_img[i] * sl_intensity   
     
     scanline_img = cv2.resize(scanline_img, (w, h))
     return scanline_img
 
-def filmGrain (img):
-    #implementar
-    return img
+def filmGrain(u, grain_amount, grain_size, grain_intensity):
+    m, n, c = u.shape
+    grain_image = np.zeros((m, n, c))
+
+    # Cria grid de índices para as dimensões da imagem
+    y_indices, x_indices = np.ogrid[:m, :n]
+    
+    num_grains = int(grain_amount * m * n)
+
+    # Gera as posições e raios dos grãos aleatoriamente
+    for _ in range(num_grains):
+        # Seleciona aleatoriamente uma posição na imagem
+        x = np.random.randint(0, m)
+        y = np.random.randint(0, n)
+
+        # Gera um raio aleatório para o grão usando dist. normal centrada em grain_size
+        radius = np.random.normal(loc=grain_size, scale=grain_size / 2)
+
+        # Cria uma máscara circular com centro em (x, y) com o raio calculado
+        mask = (x_indices - y) ** 2 + (y_indices - x) ** 2 <= radius ** 2
+
+        # Intensidade do grão aleatória dentro da faixa determinada por grain_intensity
+        intensity = np.random.uniform(-grain_intensity, grain_intensity)
+        
+        # Aplica a intensidade em grain_image nas posições definidas pela máscara
+        grain_image[mask] += intensity
+
+    v = u + grain_image
+    v = np.clip(v, 0, 1.0)
+
+    return v
 
 def main():
-    input_img = cv2.imread("01.png").astype(np.float32) / 255
+    input_img = cv2.imread("05.png").astype(np.float32) / 255
 
     yiq = colorRGB2YIQ(input_img)
     yiq = chromaDephase(yiq)
@@ -133,8 +167,10 @@ def main():
     bgr = colorYIQRGB(yiq)    
     bgr = scanline(bgr)
     bgr = bloom(bgr)
-    
-    cv2.imshow("yiq", bgr)
+    bgr = filmGrain(bgr, grain_amount, grain_size, grain_intensity)
+
+    cv2.imshow("final", bgr)
+
     cv2.waitKey()
     cv2.imshow("convert", input_img)
     cv2.waitKey()
